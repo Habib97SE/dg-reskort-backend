6d3d992ca466c73d95394830e236b7488a13a3be
Habiballah Heazrehee <hahh20@student.bth.se>
2023-09-20 14:06:00 +0200

    upodated the time

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
 
-const mongoDBURI = process.env.MONGO_DB_URI;
+const mongoDBURI =
+  "mongodb+srv://habib:RAQry1Yjw9NV3dLW@cluster0.k1wy3nw.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose
   .connect(mongoDBURI)
@@ -21,7 +22,7 @@ const statSchema = new mongoose.Schema({
     required: true,
   },
   date: {
-    type: Date,
+    type: String,
     required: false,
     default: Date.now,
   },
@@ -33,6 +34,38 @@ function isValidIP(ip) {
   return regex.test(ip);
 }
 
+function convertToCustomFormat(dateString) {
+  const date = new Date(dateString);
+
+  const YYYY = date.getUTCFullYear();
+  const MM = String(date.getUTCMonth() + 1).padStart(2, "0"); // Months are 0-based in JavaScript
+  const DD = String(date.getUTCDate()).padStart(2, "0");
+
+  const HH = String(date.getUTCHours()).padStart(2, "0");
+  const MIN = String(date.getUTCMinutes()).padStart(2, "0");
+  const SS = String(date.getUTCSeconds()).padStart(2, "0");
+
+  return `${YYYY}-${MM}-${DD} ${HH}:${MIN}:${SS}`;
+}
+
+async function hasIPBeenLoggedToday(ip) {
+  const startOfDay = new Date();
+  startOfDay.setHours(0, 0, 0, 0);
+
+  const endOfDay = new Date();
+  endOfDay.setHours(23, 59, 59, 999);
+
+  const existingEntry = await Stat.findOne({
+    ipAddress: ip,
+    date: {
+      $gte: startOfDay,
+      $lte: endOfDay,
+    },
+  });
+
+  return !!existingEntry;
+}
+
 const Stat = mongoose.model("stats", statSchema);
 
 app.use(cors());
@@ -41,6 +74,16 @@ app.use(express.json());
 app.post("/visit", async (req, res) => {
   try {
     if (isValidIP(req.body.ipAddress)) {
+      if (await hasIPBeenLoggedToday(req.body.ipAddress)) {
+        return res
+          .status(409)
+          .send({ message: "IP already logged for today." });
+      }
+      if (req.body.date) {
+        req.body.date = convertToCustomFormat(req.body.date);
+      } else {
+        req.body.date = convertToCustomFormat(new Date());
+      }
       const stat = new Stat(req.body);
       let result = await stat.save();
       if (result) {
